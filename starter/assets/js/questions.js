0eb156293ff54bf1537aec10b6e8a44da6ee9e04
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-02-25 09:05:38 +0000

    questions.js file updated to js folder

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,29 @@
+let questions = [
+    {
+        title: 'Which is not a primitive data type in javascript:',
+        choices: ['Boolean', 'String', 'Skrilla'],
+        answer: 'Skrilla'
+    },
+    {
+        title: 'Which is not a type of error in JavaScript:',
+        choices: ['Logical', 'Stringify', 'Syntax'],
+        answer: 'Stringify'
+    },
+    {
+        title: 'Arrayys in JavaScript can be used to store:',
+        choices: [
+            'numbers and strings', 
+            'other arrays', 
+            'all of the above',
+         ],
+            answer: 'all of the above',
+        
+    },
+    {
+        title: 'Which is not a data type in javascript:',
+        choices: ['Boolean', 'String', 'Skrilla'],
+        answer: 'Skrilla'
+    },
+    
+    
+]
